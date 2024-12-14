4db270fe971443179fc35f2a155c7cef8ff65cc6
sunithvs <sunithvs2002@gmail.com>
2024-12-15 00:25:56 +0530

    fix: change port

--- api/main.py
+++ api/main.py
@@ -9,6 +9,7 @@ import uvicorn
 from fastapi import FastAPI, HTTPException, Path, Depends
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
+from fastapi.middleware.cors import CORSMiddleware  # Add this import
 
 # Import your existing modules
 from config.settings import Settings
@@ -109,6 +110,14 @@ async def fetch_user_profile(username: Annotated[str, Depends(verify_username)])
             detail=f"User {username} not found: {str(e)}"
         )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

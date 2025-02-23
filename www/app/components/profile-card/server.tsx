df5cf946fb93d63c8a36aef08274afa3639f1ae6
sunithvs <sunithvs2002@gmail.com>
2025-02-23 19:35:29 +0530

    fix(build):  issues

--- www/app/components/profile-card/server.tsx
+++ www/app/components/profile-card/server.tsx
@@ -1,10 +1,34 @@
 import ProfileCardClient from "./client";
 
-export default function ProfileCard({ name, username, avatarUrl, index }: { 
+export default function ProfileCard({ 
+  name, 
+  username, 
+  avatarUrl, 
+  bio,
+  followers,
+  following,
+  publicRepos,
+  index 
+}: { 
   name: string;
   username: string;
   avatarUrl: string;
+  bio: string;
+  followers: number;
+  following: number;
+  publicRepos: number;
   index: number;
 }) {
-  return <ProfileCardClient name={name} username={username} avatarUrl={avatarUrl} index={index} />;
+  return (
+    <ProfileCardClient 
+      name={name} 
+      username={username} 
+      avatarUrl={avatarUrl}
+      bio={bio}
+      followers={followers}
+      following={following}
+      publicRepos={publicRepos}
+      index={index}
+    />
+  );
 }

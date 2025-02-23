eec356ae54a40748f7335233e8a2d8e756434ab5
nayanprasad <nayanprasad096@gmail.com>
2025-02-23 18:11:15 +0530

    feat: updated style

--- www/components/project-card.tsx
+++ www/components/project-card.tsx
@@ -38,36 +38,41 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
               <span className="text-sm">{language}</span>
             </div>
           )}
-          <div className="flex items-center gap-2 ju">
-            <Star className="w-4 h-4 fill-current" />
-            <span className="text-sm">{stars}</span>
-          </div>
-          {forks > 0 && (
-            <div className="flex items-center gap-2">
-              <GitFork className="w-4 h-4" />
-              <span className="text-sm">{forks}</span>
-            </div>
-          )}
         </div>
 
-        <div className="flex gap-2">
-          <Link
-            href={url}
-            target="_blank"
-            className="p-2 bg-[#B9FF66] rounded-lg border-2 border-black hover:bg-[#a5e65c] transition-colors"
-          >
-            <Github className="w-5 h-5" />
-          </Link>
-          {homepage && (
-            <Link
-              href={homepage}
-              target="_blank"
-              className="p-2 bg-white rounded-lg border-2 border-black hover:bg-gray-50 transition-colors"
-            >
-              <ExternalLink className="w-5 h-5" />
-            </Link>
-          )}
-        </div>
+          <div className={"flex flex-col gap-2"}>
+              <div className={"flex gap-1"}>
+                  <div className="flex items-center gap-2 ju">
+                      <Star className="w-4 h-4 fill-current"/>
+                      <span className="text-sm">{stars}</span>
+                  </div>
+                  {forks > 0 && (
+                      <div className="flex items-center gap-2">
+                          <GitFork className="w-4 h-4"/>
+                          <span className="text-sm">{forks}</span>
+                      </div>
+                  )}
+              </div>
+              <div className="flex gap-2">
+                  <Link
+                      href={url}
+                      target="_blank"
+                      className="p-2 bg-[#B9FF66] rounded-lg border-2 border-black hover:bg-[#a5e65c] transition-colors"
+                  >
+                      <Github className="w-5 h-5"/>
+                  </Link>
+                  {homepage && (
+                      <Link
+                          href={homepage}
+                          target="_blank"
+                          className="p-2 bg-white rounded-lg border-2 border-black hover:bg-gray-50 transition-colors"
+                      >
+                          <ExternalLink className="w-5 h-5"/>
+                      </Link>
+                  )}
+              </div>
+          </div>
+
       </div>
     </div>
   );

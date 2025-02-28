dee0c3cdbab700b168df99abaa697b867bd2884d
nayanprasad <nayanprasad096@gmail.com>
2025-03-01 03:24:29 +0530

    fix: params usage and linkdin api incorrect body usage

--- www/app/[username]/page.tsx
+++ www/app/[username]/page.tsx
@@ -2,13 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import {
   ArrowDown,
-  User,
   Github,
+  Globe,
   Linkedin,
   Twitter,
-  Globe,
-  Download
-} from 'lucide-react';
+  User,
+} from "lucide-react";
 import ResumeDownloadButton from "@/components/ResumeDownloadButton";
 import ProjectCard from "@/components/project-card";
 import Timeline, { transformLinkedInData } from "@/components/timeline";
@@ -16,33 +15,52 @@ import { ProfileSkeleton } from "@/components/skeletons/profile-skeleton";
 import { ProjectListSkeleton } from "@/components/skeletons/project-skeleton";
 import { TimelineSkeleton } from "@/components/skeletons/timeline-skeleton";
 import { AboutSkeleton } from "@/components/skeletons/about-skeleton";
-import { getUserProfile, getUserProjects, getUserLinkedInProfile } from "@/lib/api";
+import {
+  getUserLinkedInProfile,
+  getUserProfile,
+  getUserProjects,
+} from "@/lib/api";
 
 const iconComponents = {
-  'linkedin': Linkedin,
-  'twitter': Twitter,
-  'generic': Globe,
+  linkedin: Linkedin,
+  twitter: Twitter,
+  generic: Globe,
 };
 
 const extractDomainName = (url: string) => {
   try {
     const domain = new URL(url).hostname;
-    return domain.replace(/^www\./, '');
+    return domain.replace(/^www\./, "");
   } catch {
     return url;
   }
 };
 
-export default async function Page({ params }: { params: { username: string } }) {
-  const [user, userProjects, linkedInData] = await Promise.all([
-    getUserProfile(params.username),
-    getUserProjects(params.username),
-    getUserLinkedInProfile(params.username)
+export default async function Page({
+  params,
+}: {
+  params: { username: string };
+}) {
+  const username = params.username;
+
+  const user = await getUserProfile(username);
+
+  const linkedInAccount = user?.social_accounts?.find(
+    (e) => e.provider === "linkedin",
+  );
+  const linkedInUsername = linkedInAccount?.url?.split("/").pop() || "";
+
+  const [userProjects, linkedInData] = await Promise.all([
+    getUserProjects(username),
+    linkedInUsername ? getUserLinkedInProfile(linkedInUsername) : null,
   ]);
 
   return (
     <div className="container mx-auto px-4 py-8 flex flex-col lg:flex-row gap-8">
-      <div id="left-section" className="w-full lg:w-[400px] lg:sticky lg:top-8 lg:self-start space-y-2">
+      <div
+        id="left-section"
+        className="w-full lg:w-[400px] lg:sticky lg:top-8 lg:self-start space-y-2"
+      >
         <Link href="/" className="block">
           <Image
             src="/images/logo-full.png"
@@ -52,7 +70,7 @@ export default async function Page({ params }: { params: { username: string } })
             className="h-10 w-auto"
           />
         </Link>
-      
+
         {!user ? (
           <ProfileSkeleton />
         ) : (
@@ -70,10 +88,12 @@ export default async function Page({ params }: { params: { username: string } })
                 </div>
               </div>
             </div>
-            
+
             <div className="px-8 pb-4 pt-20">
               <div className="flex flex-col items-start text-left">
-                <h2 className="font-bold text-2xl mb-2 text-black">{user.name.toUpperCase()}</h2>
+                <h2 className="font-bold text-2xl mb-2 text-black">
+                  {user.name.toUpperCase()}
+                </h2>
                 <p className="text-gray-700 text-md">{user.bio}</p>
               </div>
 
@@ -84,24 +104,32 @@ export default async function Page({ params }: { params: { username: string } })
                 </div>
                 <div className="flex flex-wrap gap-3">
                   <a
-                    href={`https://github.com/${params.username}`}
+                    href={`https://github.com/${username}`}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="group relative w-12 h-12 flex items-center justify-center bg-white rounded-2xl border-[1px] border-black hover:bg-[#B9FF66] transition-all duration-300"
                     title="GitHub"
                   >
                     <span className="w-6 h-6 group-hover:rotate-12 transition-transform duration-300">
-                      <Github size={24} strokeWidth={2} className="text-black" />
+                      <Github
+                        size={24}
+                        strokeWidth={2}
+                        className="text-black"
+                      />
                     </span>
                     <span className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-black text-white text-xs py-1 px-2 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap z-10">
                       GitHub
                     </span>
                   </a>
-                  {user.social_accounts.map((account) => {
-                    if (account.provider.toLowerCase() === 'github') return null;
+                  {user.social_accounts?.map((account) => {
+                    if (account.provider.toLowerCase() === "github")
+                      return null;
                     const provider = account.provider.toLowerCase();
-                    const tooltipText = account.provider === 'generic' ? extractDomainName(account.url) : account.provider;
-                    
+                    const tooltipText =
+                      account.provider === "generic"
+                        ? extractDomainName(account.url)
+                        : account.provider;
+
                     return (
                       <a
                         key={account.url}
@@ -111,13 +139,27 @@ export default async function Page({ params }: { params: { username: string } })
                         className="group relative w-12 h-12 flex items-center justify-center bg-white rounded-2xl border-[1px] border-black hover:bg-[#B9FF66] transition-all duration-300"
                         title={tooltipText}
                       >
-                        {account.url.includes('devb.io') ? (
-                          <Image src="/images/logo.png" alt="devb.io" width={24} height={24} />
+                        {account.url.includes("devb.io") ? (
+                          <Image
+                            src="/images/logo.png"
+                            alt="devb.io"
+                            width={24}
+                            height={24}
+                          />
                         ) : (
                           <span className="w-6 h-6 group-hover:rotate-12 transition-transform duration-300">
                             {(() => {
-                              const IconComponent = iconComponents[provider as keyof typeof iconComponents] || iconComponents.generic;
-                              return <IconComponent size={24} strokeWidth={2} className="text-black" />;
+                              const IconComponent =
+                                iconComponents[
+                                  provider as keyof typeof iconComponents
+                                ] || iconComponents.generic;
+                              return (
+                                <IconComponent
+                                  size={24}
+                                  strokeWidth={2}
+                                  className="text-black"
+                                />
+                              );
                             })()}
                           </span>
                         )}
@@ -127,25 +169,32 @@ export default async function Page({ params }: { params: { username: string } })
                       </a>
                     );
                   })}
-                  <ResumeDownloadButton username={params.username} />
+                  <ResumeDownloadButton username={username} />
                 </div>
               </div>
 
-              {user.achievements?.total_contributions && ( <div className="mt-4">
-                <h2 className="font-bold mb-4">{user.achievements?.total_contributions } Contributions</h2>
-                <div className=" overflow-hidden">
-                  <div className="relative w-full" style={{ height: '100px' }}>
-                    <img
-                      className="absolute top-[30%] left-1/2 transform -translate-x-124 -translate-y-1/2 scale-[1]"
-                      src={`https://ghchart.rshah.org/5F8417/${user.username}`}
-                      alt={`${user.name}'s GitHub contributions`}
-                      style={{
-                        maxWidth: 'none'
-                      }}
-                    />
+              {user.achievements?.total_contributions > 0 && (
+                <div className="mt-4">
+                  <h2 className="font-bold mb-4">
+                    {user.achievements?.total_contributions} Contributions
+                  </h2>
+                  <div className="overflow-hidden">
+                    <div
+                      className="relative w-full"
+                      style={{ height: "100px" }}
+                    >
+                      <img
+                        className="absolute top-[30%] left-1/2 transform -translate-x-124 -translate-y-1/2 scale-[1]"
+                        src={`https://ghchart.rshah.org/5F8417/${user.username}`}
+                        alt={`${user.name}'s GitHub contributions`}
+                        style={{
+                          maxWidth: "none",
+                        }}
+                      />
+                    </div>
                   </div>
                 </div>
-              </div>)}
+              )}
             </div>
           </div>
         )}
@@ -165,7 +214,7 @@ export default async function Page({ params }: { params: { username: string } })
                 <div className="bg-white rounded-xl p-6 border-1 border-black border-b-4">
                   <h2 className="text-xl font-bold mb-4">💻 Languages</h2>
                   <div className="flex flex-wrap gap-3">
-                    {userProjects?.top_languages.map((language, index) => (
+                    {userProjects?.top_languages?.map((language, index) => (
                       <span
                         key={index}
                         className="px-4 py-2 bg-gray-100 rounded-lg border-black"
@@ -211,8 +260,7 @@ export default async function Page({ params }: { params: { username: string } })
           {!linkedInData ? (
             <TimelineSkeleton />
           ) : (
-            linkedInData &&
-            linkedInData.experience.length > 0 && (
+            linkedInData.experience?.length > 0 && (
               <>
                 <h2 className="text-2xl font-bold mb-8">
                   Experience <ArrowDown strokeWidth={2} className="inline" />
@@ -230,8 +278,7 @@ export default async function Page({ params }: { params: { username: string } })
           {!linkedInData ? (
             <TimelineSkeleton />
           ) : (
-            linkedInData &&
-            linkedInData.education.length > 0 && (
+            linkedInData.education?.length > 0 && (
               <>
                 <h2 className="text-2xl font-bold mb-8">
                   Education <ArrowDown strokeWidth={2} className="inline" />
@@ -253,7 +300,7 @@ export default async function Page({ params }: { params: { username: string } })
             <ProjectListSkeleton />
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              {userProjects?.top_projects.map((project) => (
+              {userProjects?.top_projects?.map((project) => (
                 <ProjectCard key={project.name} {...project} />
               ))}
             </div>

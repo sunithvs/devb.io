1eeede64b25836e211844af23eed1f17bfc0a020
sunith vs <sunithvs2002@gmail.com>
2025-11-24 08:59:18 +0530

    feat: github login

--- web/app/signin/page.tsx
+++ web/app/signin/page.tsx
@@ -9,22 +9,21 @@ import { toast } from "@/components/ui/use-toast";
 import { useSearchParams } from "next/navigation";
 
 export default function SignInPage() {
-  const [isGoogleLoading, setIsGoogleLoading] = useState<boolean>(false);
+  const [isGitHubLoading, setIsGitHubLoading] = useState<boolean>(false);
   const supabase = createClient();
 
   const searchParams = useSearchParams();
 
   const next = searchParams.get("next");
 
-  async function signInWithGoogle() {
-    setIsGoogleLoading(true);
+  async function signInWithGitHub() {
+    setIsGitHubLoading(true);
     try {
       const { error } = await supabase.auth.signInWithOAuth({
-        provider: "google",
+        provider: "github",
         options: {
-          redirectTo: `${window.location.origin}/auth/callback${
-            next ? `?next=${encodeURIComponent(next)}` : ""
-          }`,
+          redirectTo: `${window.location.origin}/auth/callback${next ? `?next=${encodeURIComponent(next)}` : ""
+            }`,
         },
       });
 
@@ -34,10 +33,10 @@ export default function SignInPage() {
     } catch (error) {
       toast({
         title: "Please try again.",
-        description: "There was an error logging in with Google.",
+        description: "There was an error logging in with GitHub.",
         variant: "destructive",
       });
-      setIsGoogleLoading(false);
+      setIsGitHubLoading(false);
     }
   }
 
@@ -45,15 +44,15 @@ export default function SignInPage() {
     <Button
       type="button"
       variant="outline"
-      onClick={signInWithGoogle}
-      disabled={isGoogleLoading}
+      onClick={signInWithGitHub}
+      disabled={isGitHubLoading}
     >
-      {isGoogleLoading ? (
+      {isGitHubLoading ? (
         <Icons.loaderCircle className="mr-2 size-4 animate-spin" />
       ) : (
-        <Icons.google className="mr-2 size-6" />
+        <Icons.gitHub className="mr-2 size-6" />
       )}{" "}
-      Sign in with Google
+      Sign in with GitHub
     </Button>
   );
 }
